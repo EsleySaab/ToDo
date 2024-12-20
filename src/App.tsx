2897d313538da523b33b8bfcc17797b3f70f6b89
EsleySaab <esleysantana.sa@gmail.com>
2024-12-20 12:09:04 -0300

    feat: creating the project and adding styles and global variables

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.css"
+
+import "./global.css"
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
