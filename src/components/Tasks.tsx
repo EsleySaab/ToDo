7b60ced30a9a28033e3f7a57cf6c239a97974636
EsleySaab <esleysantana.sa@gmail.com>
2024-12-20 19:25:58 -0300

    style: adjusting the tasks component

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -4,14 +4,28 @@ import { PlusCircle } from "phosphor-react"
 
 export function Tasks() {
   return (
-    <form className={styles.tasks}>
-      <div className={styles.content}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
-        <button type="submit">
-          <span>Criar</span>
-          <PlusCircle />
-        </button>
+    <div>
+      <form className={styles.tasks}>
+        <div className={styles.content}>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button type="submit">
+            <span>Criar</span>
+            <PlusCircle size={18} />
+          </button>
+        </div>
+      </form>
+
+      <div className={styles.list}>
+        <div className={styles.listContent}>
+          <p>Tarefas criadas</p>
+          <span>0</span>
+        </div>
+
+        <div className={styles.secondListContent}>
+          <p>Concluídas</p>
+          <span>0</span>
+        </div>
       </div>
-    </form>
+    </div>
   )
 }
